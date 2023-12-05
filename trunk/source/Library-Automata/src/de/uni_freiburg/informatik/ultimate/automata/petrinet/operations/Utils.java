919a5e7f237b65fa1f6c2dc68ba4917f2241c7ad
d-manuel <38905215+d-manuel@users.noreply.github.com>
2023-12-05 12:38:23 +0100

    minor formatting

--- trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/petrinet/operations/Utils.java
+++ trunk/source/Library-Automata/src/de/uni_freiburg/informatik/ultimate/automata/petrinet/operations/Utils.java
@@ -3,18 +3,21 @@ package de.uni_freiburg.informatik.ultimate.automata.petrinet.operations;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.netdatastructures.Transition;
 
 public final class Utils {
-	private Utils() {};
+
+	private Utils() {
+	};
+
 	/*
 	 * Properly generate a String for a Petri Transition
 	 */
-	public static <LETTER, PLACE> String transitionToString(Transition<LETTER, PLACE> trans) {
+	public static <LETTER, PLACE> String transitionToString(final Transition<LETTER, PLACE> trans) {
 		String res = "{";
 		res += "{";
-		for (var pre : trans.getPredecessors()) {
+		for (final var pre : trans.getPredecessors()) {
 			res += " " + pre.toString() + " ";
 		}
 		res += "} " + trans.getSymbol() + " {";
-		for (var suc : trans.getSuccessors()) {
+		for (final var suc : trans.getSuccessors()) {
 			res += " " + suc.toString() + " ";
 		}
 		res += "}}";
