5bff154f9b9a1e0d4cb3fb52b174aa12b0fb180b
d-manuel <38905215+d-manuel@users.noreply.github.com>
2023-12-13 17:33:03 +0100

    More personal tests

--- trunk/source/Library-AutomataTest/src/de/uni_freiburg/informatik/ultimate/petrinet/BuchiPetriTest.java
+++ trunk/source/Library-AutomataTest/src/de/uni_freiburg/informatik/ultimate/petrinet/BuchiPetriTest.java
@@ -14,6 +14,8 @@ import de.uni_freiburg.informatik.ultimate.automata.petrinet.PetriNetLassoRun;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.PetriNetNot1SafeException;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.netdatastructures.BoundedPetriNet;
 import de.uni_freiburg.informatik.ultimate.automata.petrinet.operations.BuchiPetriNet2FiniteAutomaton;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.operations.RemoveDead;
+import de.uni_freiburg.informatik.ultimate.automata.petrinet.operations.RemoveDeadBuchi;
 import de.uni_freiburg.informatik.ultimate.automata.statefactory.StringFactory;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger;
 import de.uni_freiburg.informatik.ultimate.core.model.services.ILogger.LogLevel;
@@ -38,7 +40,7 @@ public class BuchiPetriTest {
 
 	@Test
 	public void test1() throws AutomataLibraryException {
-		final Set<String> alphabet = Set.of("a", "b", "c", "d");
+		final Set<String> alphabet = Set.of("a", "b", "c", "d", "e");
 		final BoundedPetriNet<String, String> petriNet = new BoundedPetriNet<>(mServices, alphabet, false);
 		petriNet.addPlace("p1", true, false);
 		petriNet.addPlace("p2", false, false);
@@ -46,14 +48,19 @@ public class BuchiPetriTest {
 		petriNet.addPlace("p4", false, false);
 		petriNet.addPlace("p5", false, true);
 		petriNet.addPlace("p6", false, false);
+		petriNet.addPlace("p7", false, false);
 		petriNet.addTransition("a", ImmutableSet.of(Set.of("p1")), ImmutableSet.of(Set.of("p2", "p3", "p4")));
 		petriNet.addTransition("b", ImmutableSet.of(Set.of("p2", "p3", "p4")), ImmutableSet.of(Set.of("p5", "p6")));
 		petriNet.addTransition("c", ImmutableSet.of(Set.of("p5")), ImmutableSet.of(Set.of("p2", "p3")));
 		petriNet.addTransition("d", ImmutableSet.of(Set.of("p6")), ImmutableSet.of(Set.of("p4")));
 
+		petriNet.addTransition("e", ImmutableSet.of(Set.of("p6", "p5")), ImmutableSet.of(Set.of("p7")));
+
 		final IPetriNet<String, String> abstraction = petriNet;
 
-		check(abstraction, false);
+		checkEmptinessCorrect(abstraction, false);
+
+		// reduceAbstraction(abstraction);
 	}
 
 	@Test
@@ -74,7 +81,7 @@ public class BuchiPetriTest {
 
 		final IPetriNet<String, String> abstraction = petriNet;
 
-		check(abstraction, true);
+		checkEmptinessCorrect(abstraction, true);
 	}
 
 	@Test
@@ -93,10 +100,52 @@ public class BuchiPetriTest {
 
 		final IPetriNet<String, String> abstraction = petriNet;
 
-		check(abstraction, false);
+		checkEmptinessCorrect(abstraction, false);
 	}
 
-	private void check(final IPetriNet<String, String> abstraction, final boolean expectedEmpty)
+	@Test
+	public void test4() throws AutomataLibraryException {
+		final Set<String> alphabet = Set.of("a");
+		final BoundedPetriNet<String, String> petriNet = new BoundedPetriNet<>(mServices, alphabet, false);
+		petriNet.addPlace("p1", true, false);
+		petriNet.addPlace("p2", false, false);
+		petriNet.addPlace("p3", false, false);
+		petriNet.addPlace("p4", false, true);
+		petriNet.addTransition("a", ImmutableSet.of(Set.of("p1")), ImmutableSet.of(Set.of("p2", "p3")));
+		petriNet.addTransition("a", ImmutableSet.of(Set.of("p2", "p3")), ImmutableSet.of(Set.of("p4")));
+		petriNet.addTransition("a", ImmutableSet.of(Set.of("p4")), ImmutableSet.of(Set.of("p4")));
+
+		final IPetriNet<String, String> abstraction = petriNet;
+
+		checkEmptinessCorrect(abstraction, false);
+
+		// reduceAbstraction(petriNet);
+
+	}
+
+	@Test
+	public void test5() throws AutomataLibraryException {
+		final Set<String> alphabet = Set.of("a");
+		final BoundedPetriNet<String, String> petriNet = new BoundedPetriNet<>(mServices, alphabet, false);
+		petriNet.addPlace("p1", true, false);
+		petriNet.addPlace("p2", false, false);
+		petriNet.addPlace("p3", false, true);
+		petriNet.addPlace("p4", false, false);
+		petriNet.addPlace("p5", true, false);
+		petriNet.addTransition("a", ImmutableSet.of(Set.of("p1")), ImmutableSet.of(Set.of("p2")));
+		petriNet.addTransition("a", ImmutableSet.of(Set.of("p2", "p5")), ImmutableSet.of(Set.of("p3", "p4")));
+		petriNet.addTransition("a", ImmutableSet.of(Set.of("p4")), ImmutableSet.of(Set.of("p1")));
+		petriNet.addTransition("a", ImmutableSet.of(Set.of("p3")), ImmutableSet.of(Set.of("p5")));
+
+		final IPetriNet<String, String> abstraction = petriNet;
+
+		checkEmptinessCorrect(abstraction, false);
+
+		reduceAbstraction(petriNet);
+
+	}
+
+	private void checkEmptinessCorrect(final IPetriNet<String, String> abstraction, final boolean expectedEmpty)
 			throws PetriNetNot1SafeException, AutomataOperationCanceledException {
 		// Check Emptiness by Conversion to BüchiAutomaton
 		final var automaton =
@@ -124,5 +173,15 @@ public class BuchiPetriTest {
 			mLogger.info(run2.getStem());
 			mLogger.info(run2.getLoop());
 		}
+	}
+
+	private void reduceAbstraction(final IPetriNet<String, String> petriNet)
+			throws AutomataOperationCanceledException, PetriNetNot1SafeException {
+		// final var t = new RemoveUnreachable<>(mServices, petriNet);
+		// final var s = new RemoveDead<>(mServices, petriNet);
+		final var r = new RemoveDeadBuchi<>(mServices, petriNet, null).getResult();
+		final var q = new RemoveDead<>(mServices, petriNet);
+		mLogger.info("end");
+		// mLogger.info(s);
 	}
 }
